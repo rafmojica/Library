8846dc6da777583eae8405252a6b1d37f8f2c2b7
rafa <rafumojica1@gmail.com>
2022-09-15 11:45:41 +0800

    added more features

---
+++ books.js
@@ -0,0 +1,125 @@
+function renderBooks() {
+  const booksWrapper = document.querySelector('.latest__books')
+  
+  booksWrapper.innerHTML =
+  `<div class="book">
+  <figure class="book__img--wrapper">
+    <img src="./assets/book-1.jpeg" alt="" class="book__img" />
+  </figure>
+  <div class="book__title">The 10X Rule</div>
+  <div class="book__ratings">
+    <i class="fas fa-star"></i>
+    <i class="fas fa-star"></i>
+    <i class="fas fa-star"></i>
+    <i class="fas fa-star"></i>
+    <i class="fas fa-star-half-alt"></i>
+  </div>
+  <div class="book__price">$19.99</div>
+</div>`
+}
+
+setTimeout(() => {
+  renderBooks()
+});
+
+// FAKE DATA
+function getBooks() {
+  return [
+    {
+      id: 1,
+      title: "Crack the Coding Interview",
+      url: "assets/crack the coding interview.png",
+      originalPrice: 49.95,
+      salePrice: 14.95,
+      rating: 4.5,
+    },
+    {
+      id: 2,
+      title: "Atomic Habits",
+      url: "assets/atomic habits.jpg",
+      originalPrice: 39,
+      salePrice: null,
+      rating: 5,
+    },
+    {
+      id: 3,
+      title: "Deep Work",
+      url: "assets/deep work.jpeg",
+      originalPrice: 29,
+      salePrice: 12,
+      rating: 5,
+    },
+    {
+      id: 4,
+      title: "Ikigai",
+      url: "assets/IKIGAI.jpg",
+      originalPrice: 29,
+      salePrice: 12,
+      rating: 5,
+    },
+    {
+      id: 5,
+      title: "The 10X Rule",
+      url: "assets/book-1.jpeg",
+      originalPrice: 44,
+      salePrice: 19,
+      rating: 4.5,
+    },
+    {
+      id: 6,
+      title: "Be Obsessed Or Be Average",
+      url: "assets/book-2.jpeg",
+      originalPrice: 32,
+      salePrice: 17,
+      rating: 4,
+    },
+    {
+      id: 7,
+      title: "Rich Dad Poor Dad",
+      url: "assets/book-3.jpeg",
+      originalPrice: 70,
+      salePrice: 12.5,
+      rating: 5,
+    },
+    {
+      id: 8,
+      title: "Cashflow Quadrant",
+      url: "assets/book-4.jpeg",
+      originalPrice: 11,
+      salePrice: 10,
+      rating: 4,
+    },
+    {
+      id: 9,
+      title: "48 Laws of Power",
+      url: "assets/book-5.jpeg",
+      originalPrice: 38,
+      salePrice: 17.95,
+      rating: 4.5,
+    },
+    {
+      id: 10,
+      title: "The 5 Second Rule",
+      url: "assets/book-6.jpeg",
+      originalPrice: 35,
+      salePrice: null,
+      rating: 4,
+    },
+    {
+      id: 11,
+      title: "Your Next Five Moves",
+      url: "assets/book-7.jpg",
+      originalPrice: 40,
+      salePrice: null,
+      rating: 4,
+    },
+    {
+      id: 12,
+      title: "Mastery",
+      url: "assets/book-8.jpeg",
+      originalPrice: 30,
+      salePrice: null,
+      rating: 4.5,
+    },
+  ];
+}
